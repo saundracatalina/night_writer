adc6c6ef80a3209c9fec0624a492ecd42b6e18b5
Saundra <saundracatalina@gmail.com>
2020-11-07 15:12:07 -0700

    Add test length of braille chars

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -13,6 +13,19 @@ class AlphabetTest < Minitest::Test
     alphabet = Alphabet.new
 
     expected_a = ["0.\n", "..\n", ".."]
+    expected_b = ["0.\n", "0.\n", ".."]
+    expected_x = ["00\n", "..\n", "00"]
+    expected_z = ["0.\n", ".0\n", "00"]
+
     assert_equal expected_a, alphabet.characters["a"]
+    assert_equal expected_b, alphabet.characters["b"]
+    assert_equal expected_x, alphabet.characters["x"]
+    assert_equal expected_z, alphabet.characters["z"]
+  end
+
+  def test_length_of_braille_chars
+    alphabet = Alphabet.new
+    
+    assert_equal 3, alphabet.characters["a"].length
   end
 end
