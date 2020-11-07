524cf63a2f34c71b39d3d662941747627971c9c6
Saundra <saundracatalina@gmail.com>
2020-11-07 14:12:59 -0700

    Add test_eng_to_braille

--- test/alphabet_test.rb
+++ test/alphabet_test.rb
@@ -6,6 +6,13 @@ class AlphabetTest < Minitest::Test
   def test_it_exists
     alphabet = Alphabet.new
 
-    assert_instance_of Alphabet, alphabet 
+    assert_instance_of Alphabet, alphabet
+  end
+
+  def test_eng_to_braille
+    alphabet = Alphabet.new
+
+    expected = ["0.\n", "..\n", ".."]
+    assert_equal expected, alphabet.characters["a"]
   end
 end
