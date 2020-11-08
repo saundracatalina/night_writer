3135ca8aec98139fc097e955c2195fd8b41b05aa
Saundra <saundracatalina@gmail.com>
2020-11-08 11:39:51 -0700

    Add message_to_terminal method

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,26 +1,34 @@
 require "./lib/translator"
-require "./lib/alphabet"
 
 class NightWriter
-  attr_reader :translator,
-              :alphabet
+  attr_reader :translator
+
 
   def initialize
     @translator = Translator.new
-    @alphabet = Alphabet.new
   end
 
   def read_and_write_new_file
     message = File.open(ARGV[0], "r")
-    english = message.readlines
+    english = message.read
     message.close
-
     translated_message = @translator.eng_to_braille(english)
     braille = File.open(ARGV[1], "w")
     braille.write(translated_message)
     braille.close
   end
+
+  def message_to_terminal
+    lines = File.readlines(ARGV[1])
+    line_content = lines.map do |line|
+      line.chomp
+    end
+    text = line_content.join
+    text_count = text.size
+    puts "Created #{ARGV[1].inspect} containing #{text_count} characters"
+  end
 end
 
 nightwriter = NightWriter.new
 nightwriter.read_and_write_new_file
+nightwriter.message_to_terminal
