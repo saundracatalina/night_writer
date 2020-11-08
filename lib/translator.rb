374b25478719093fe1c51a0c5933cdd6fb9ded0f
Saundra <saundracatalina@gmail.com>
2020-11-07 19:56:50 -0700

    Add eng_to_braille translation method

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,4 +6,19 @@ class Translator
   def initialize
     @alphabet = Alphabet.new
   end
+
+  def eng_to_braille(words)
+    characters = words.chars
+    first_row = []
+    second_row = []
+    third_row = []
+    characters.each do |character|
+      @alphabet.letters.each do |letter, braille|
+        first_row << braille[0] if letter == character
+        second_row << braille[1] if letter == character
+        third_row << braille[2] if letter == character
+      end
+    end
+    braille_type = "#{first_row.join}\n#{second_row.join}\n#{third_row.join}"
+  end
 end
