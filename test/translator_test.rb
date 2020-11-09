f16bf847a84b1b08b3fe22e492ada33be67c271f
Saundra <saundracatalina@gmail.com>
2020-11-09 14:59:14 -0700

    Add test_can_translate_braille_to_eng

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -59,6 +59,19 @@ class TranslatorTest < Minitest::Test
     assert_equal expected_sentence, translator.eng_to_braille("hello world")
   end
 
+  def test_can_translate_braille_to_eng
+    translator = Translator.new
+
+    braille_a = "0.\n..\n.."
+    assert_equal "a", translator.braille_to_eng(braille_a)
+
+    braille_hello = "0.0.0.0.0.\n00.00.0..0\n....0.0.0."
+    assert_equal "hello", translator.braille_to_eng(braille_hello)
+
+    braille_hello_world = "0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0..."
+    assert_equal "hello world", translator.braille_to_eng(braille_hello_world)
+  end
+
   # def test_can_insert_line_breaks
   #   translator = Translator.new
   #   translator.eng_to_braille("aaa")
