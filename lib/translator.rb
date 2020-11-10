1f108061948ce4002db910ef143a62b5a33b4e72
Saundra <saundracatalina@gmail.com>
2020-11-09 22:08:35 -0700

    Remove unused methods

--- lib/translator.rb
+++ lib/translator.rb
@@ -46,8 +46,6 @@ class Translator
   #   braille.insert(13, "\n")
   # end
 
-
-
   # def insert_line_breaks
   #   require "pry"; binding.pry
   #   if join_file_lines(ARGV[1]).size % 12 == 0
@@ -55,30 +53,6 @@ class Translator
   #   else
   #     "#{first_row.join}\n#{second_row.join}\n#{third_row.join}"
   #   end
-  # end
-
-  # join_file_lines(ARGV[1])
-  # => "0.0.0............."
-
-  # join_file_lines(ARGV[1]).size
-  # => 18
-  #
-  # join_file_lines(ARGV[1])[0]
-  # => "0"
-
-  # def eng_to_braille(words)
-  #   characters = words[0..-1].chomp.chars
-  #   first_row = []
-  #   second_row = []
-  #   third_row = []
-  #   characters.each do |character|
-  #     @alphabet.letters.map do |letter, braille|
-  #         first_row << braille[0] if letter == character
-  #         second_row << braille[1] if letter == character
-  #         third_row << braille[2] if letter == character
-  #     end
-  #   end
-  #   braille_type = "#{first_row.join}\n#{second_row.join}\n#{third_row.join}"
   # end
 
   def join_file_lines(file)
