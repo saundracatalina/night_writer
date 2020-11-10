79a7397d5b3e068d3645e6cbc38d770cda3723e1
Saundra <saundracatalina@gmail.com>
2020-11-09 22:35:01 -0700

    Add test setup

---
+++ test/night_reader_test.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/night_reader"
+require "./lib/translator"
+
+class NightReaderTest < Minitest::Test
+
+end
---
+++ test/night_writer_test.rb
@@ -0,0 +1,8 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/night_writer"
+require "./lib/translator"
+
+class NightWriterTest < Minitest::Test
+
+end
